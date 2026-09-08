a22dd3c1e7c96c3814c1e26577d01b20a2a81fd5
mukherr12 <mukherr@icloud.com>
2026-09-08 12:53:25 +0100

    adding thins

--- lower-8th/CSharp/test-080926/obj/Debug/net10.0/test-080926.AssemblyInfo.cs
+++ lower-8th/CSharp/test-080926/obj/Debug/net10.0/test-080926.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test-080926")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21c1d879fdeb659fbde37976115f4358bfb900a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985d91a33afae11d29ede271b9db6dab86c1370b")]
 [assembly: System.Reflection.AssemblyProductAttribute("test-080926")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test-080926")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
