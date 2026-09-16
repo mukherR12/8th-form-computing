26ee8bde01a14ca921efeee00b326f1af061a862
mukherr12 <mukherr@icloud.com>
2026-09-16 12:05:50 +0100

    test if onedrive works

--- lower-8th/CSharp/Introduction_to_CSharp/obj/Debug/net10.0/Introduction_to_CSharp.AssemblyInfo.cs
+++ lower-8th/CSharp/Introduction_to_CSharp/obj/Debug/net10.0/Introduction_to_CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Introduction_to_CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca18eefe1e5f582e16806ab8fe2e58dd6b2e1ff4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae0fa40bb8e65bb9e13aaebccc646464bb1808b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Introduction_to_CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Introduction_to_CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
